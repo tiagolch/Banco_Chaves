3987981e7355222e19f73f289437c372884c16e0
Tiago Chaves <30913654+tiagolch@users.noreply.github.com>
2022-05-25 12:04:22 +0000

    refatorado

--- banco.py
+++ banco.py
@@ -1,4 +1,5 @@
 from datetime import date
+import os
 print('='*30)
 print('Bem vindo ao banco Chaves')
 print('='*30)
@@ -7,37 +8,49 @@ print('='*30)
 saldo = 0.0
 extrato = []
 
+def clear():
+  return os.system('clear')
+
+def add_extrato(valor):
+  global extrato
+  extrato.append(f'Dia - {str(date.today())} Valor {str(valor)}')
+
+
 
 def consulta_saldo():
+    # clear()
     global saldo
     print(f'Seu saldo é R${saldo}')
     #return saldo
 
 
 def consulta_extrato():
+    clear()
     global extrato
     print('+'*30)
     for i in extrato:
-        print(i)
+      print(i)
+    consulta_saldo()
     print('+' * 30)
 
 
-def deposito(x):
-    if x < 0:
-        print(f'{x} Não é um deposito valido')
+def deposito(valor):
+    clear()
+    if valor < 0:
+        print(f'{valor} Não é um deposito valido')
     else:
         global saldo
-        saldo += x
-        extrato.append(f'Dia - {str(date.today())} Valor +{str(valor)} -- Saldo {str(saldo)}')
+        saldo += valor
+        add_extrato(f'+{valor}')
         return saldo
 
 
 def saque(valor):
+    clear()
     global saldo
-    global extrato
-    if int(valor) <= saldo:
+    if float(valor) <= saldo:
         saldo -= valor
-        extrato.append(f'Dia - {str(date.today())} Valor -{str(valor)} -- Saldo {str(saldo)}')
+        add_extrato(f'-{valor}')
     else:
         print(f'Não ha saldo suficiente, saldo atual R${saldo}')
 
@@ -58,20 +71,30 @@ while opcao != 5:
     opcao = int(input('Opção => '))
 
     if opcao == 1:
-        valor = float(input('Qual valor para Deposito? R$'))
-        saldo = deposito(valor)
-
+      while True:
+        try:
+          valor = float(input('Qual valor para Deposito? R$'))
+          if valor > 0:
+            saldo = deposito(valor)
+            break
+        except:
+          print('Only positive numbers')
+          
     elif opcao == 2:
-        valor = float(input('Qual valor para Saque? R$'))
-        saque(valor)
-
+      while True:
+        try:
+          valor = float(input('Qual valor para Saque? R$'))
+          if valor > 0:
+            saque(valor)
+            break
+        except:
+          print('Only positive numbers')
     elif opcao == 3:
         consulta_saldo()
     elif opcao == 4:
         consulta_extrato()
     else:
         print('Ate logo')
-
 
 
 
